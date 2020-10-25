0250329f400158bc8ff9e7a924c466d00f332243
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2020-10-25 23:16:33 +0000

    added visitors data structure, 4 arrays in total

--- visitor backend123.js
+++ visitor backend123.js
@@ -37,3 +37,41 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 binding host and then print the message when the server starts listening
 the visitor log is an associative array or hash table or an object. 
 */
+
+/*the visitor log is an associative array or hash table or an objct
+they have key value pairs, i can think of it more like a dictionary
+linear way of storing arrays is like array[0]="Bill Hamm" 
+the value of each of these keys are associative arrays themeselves with two keys
+arrived key is the last time the visitor arrived at the office
+and it does not exist if the visitor is not in the office currently
+history key- is an array itself with values start and end times of
+previous visits and does nor exist durig the fisrt visit to the office
+unless visitor provides time they left*/
+
+// visitors data structure
+var visitors = {
+    "Bill Hamm": {
+        arrived: new Date(Date.now() - 1000*3600*2)
+    },
+    "Deborah Lapidot": {
+        arrived: new Date(Date.now() - 1000*3600),
+        history: [
+            {
+                arrived: new Date(Date.now() - 26*1000*3600),
+                left: new Date(Date.now() - 24*1000*3600)
+            }
+        ]    
+    },
+    "Ehud ben-Gera": {
+        history: [
+            {
+                arrived: new Date(Date.now() - (3*24+3)*1000*3600),
+                left: new Date(Date.now() - (3*24-1)*1000*3600)
+            },
+            {
+                arrived: new Date(Date.now() - (2*24+2)*1000*3600),
+                left: new Date(Date.now() - (2*24-5)*1000*3600)
+            }
+        ]
+    }
+};
