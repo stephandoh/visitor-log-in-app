64e52d54c5d77868a4b35ff4ea1425b28fc20ed6
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2020-10-26 17:26:11 +0000

    added user interface

--- visitor backend123.js
+++ visitor backend123.js
@@ -259,7 +259,191 @@ app.get("/hello", /* @callback */ function(req, res) {
 	res.send("Hello, world");
 });
 
+// Given a time difference in miliseconds, return a string with the approximate value
+var tdiffToString = (msec) => {
+	var sec = msec/1000;
+	
+	// Seconds
+	if (sec === 1)
+		return "one second";
+		
+	if (sec < 60)
+		return Math.floor(sec) + " second" + (sec < 2 ? "" : "s"); 
+		
+	if (sec < 3600)
+		return Math.floor(sec/60) + " minute" + (sec < 60*2 ? "" : "s");
+		
+	if (sec < 3600*24)
+		return Math.floor(sec/3600) + " hour" + (sec < 3600*2 ? "" : "s");
+	
+	return Math.floor(sec/(3600*24)) +  " day" + (sec < 3600*24*2 ? "" : "s");
+};
 
+
+
+// Given a history entry (arrived and left times), create a table row with that information
+var histEntryToRow = (entry) => {
+	return `<tr>
+		<td>${entry.arrived}</td>
+		<td>${entry.left}</td>
+		<td>${tdiffToString(entry.left-entry.arrived)}</td>
+		</tr>`;
+};
+
+
+// Given a history, create a table with it
+var histToTable = (history) => {
+	if (history === undefined)
+		return "";
+		
+	if (history.length === 0)
+		return "";
+			
+	return `<details>
+		<table border style="background-color: yellow">
+			<tr>
+				<th>
+					Arrived
+				</th>
+				<th>
+					Left
+				</th>
+				<th>
+					Time here
+				</th>
+			</tr>
+			${history.map(histEntryToRow).reduce((a, b) => a+b)}
+		</table>
+	</details>`;
+};
+
+
+
+// Given a user name, create a table row for the user
+var userToRow = (name) => {
+	var visitor = getVisitor(name);
+	
+	return `<tr>
+		<td>
+			${name}
+		</td>
+		<td>
+			${visitor.arrived !== undefined ? `Yes, for the last ${tdiffToString(Date.now()-visitor.arrived)}` : "No"}
+		</td>
+		<td>
+			${histToTable(visitor.history)}
+		</td>
+	</tr>
+		`;			
+};
+
+
+// Given a user name list, create a table for those users
+var usersToTable = (list) => {
+	if (list.length === 0)
+		return "";		
+	
+	return `
+		<table border>
+			<tr>
+				<th>
+					Name
+				</th>
+				<th>
+					Here?
+				</th>
+				<th>
+					History
+				</th>
+			</tr>
+		${list.map(userToRow).reduce((a,b) => a+b)}
+		</table>
+		`;
+};
+
+
+var visitorsHTML = () => {
+	return `
+				<h2>Full Visitor List</h2>
+					${usersToTable(visitorNames())}
+	`;
+};
+
+
+var currentVisitorsHTML = () => {
+	return `
+				<h2>Current Visitor List</h2>
+					${usersToTable(currentVisitorNames())}
+		`;
+};
+
+
+
+var loginForm = () => {
+	return `<h2>Log in a visitor</h2>
+			<form method="get" action="login">
+				Visitor to log in: <input type="text" name="user">
+			</form>`;
+};
+
+
+var logoutForm = () => {
+	if (currentVisitorNames().length === 0) 
+		return "No users to log out";
+	
+	return `
+		<h2>Log out a visitor</h2>
+		<ul>
+			${currentVisitorNames()
+				.map(name => `<li> 
+					<a href="logout?user=${encodeURI(name)}">${name}</a> 
+					</li>`)
+				.reduce((a,b) => a + b)}
+		</ul>`;	
+};
+
+
+var embedInHTML = (str) => {
+	return `<html><body>${str}</body></html>`;	
+};
+
+app.get("/visitors", (req, res) => {
+	res.send(embedInHTML(visitorsHTML()));
+});
+
+app.get("/currentVisitors", (req, res) => {
+	res.send(embedInHTML(currentVisitorsHTML()));
+});
+
+
+app.get("/login", (req, res) => {
+	if (req.query.user === undefined)
+		res.send(embedInHTML(loginForm()));
+	else 
+		res.send(logIn(req.query.user));
+});
+
+
+app.get("/logout", (req, res) => {
+	if (req.query.user === undefined)
+		res.send(embedInHTML(logoutForm()));
+	else
+		res.send(logOut(req.query.user));
+});
+
+
+
+app.get(["/index.html", "/"], (req, res) => {
+	res.send(`<html><body>
+		${loginForm()}
+		<hr />
+		${logoutForm()}
+		<hr />
+		${currentVisitorsHTML()}
+		<hr />
+		${visitorsHTML()}
+	</body></html>`);	
+});
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -269,3 +453,5 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+//  the user interface
